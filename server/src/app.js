55615cae763695c7419a7f8dbdf9190ac1f3cdff
Sammy <sammyoziegbe@gmail.com>
2023-01-14 08:00:34 +0100

    redirect all pages to index.html

--- server/src/app.js
+++ server/src/app.js
@@ -15,6 +15,9 @@ app.use(express.json())
 
 app.use(express.static(path.join(__dirname, '..', 'public')))
 
+app.get('/*', (req, res) => {
+  return res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
+})
 
 /**
  * API routes
